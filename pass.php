aeb953ac2692059482784c8ef436dbadfea70e65
lucasvelten972 <47528396+lucasvelten972@users.noreply.github.com>
2019-05-05 23:40:59 +0200

    Add files via upload

--- pass.php
+++ pass.php
@@ -1,21 +1,26 @@
 <?php
-    $login = isset($_POST["identifiant"]) ? $_POST["identifiant"] : "";
-    $pass = isset($_POST["passw"]) ? $_POST["passw"] : "";
+
+    $login = isset($_POST["uname"]) ? $_POST["uname"] : "";
+    $psw = isset($_POST["psw"]) ? $_POST["psw"] : "";
     //paire (utilisateur => mot de passe) stockés dans le serveur
+
     //on utilise 3 paires juste pour montrer un exemple
     $logs = array(
         "toto" => "totomdp",
         "titi" => "titimdp",
         "tutu" => "123tutu456",
     );
+
+
     $connexion = false;
-for ($i = 0; $i < count($logs); $i++) { if ($logs[$login] == $pass) {
-            $connexion = true;
-break; }
-}
-    if ($connexion) {
-        echo "Connexion ŕeussie.";
-} else {
-        echo "Connexion refusée.";
+    for ($i = 0; $i < count($logs); $i++) { if ($logs[$login] == $pass) {
+                $connexion = true;
+    break; }
     }
+        if ($connexion) {
+            echo "Connexion ŕeussie.";
+    } else {
+            echo "Connexion refusée.";
+        }
 ?>
+
